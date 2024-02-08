f5d94d0ccc195a26828d5d772dfacb4a2afb1a23
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2024-02-08 18:59:13 +0530

    Implemented render function to orchestrate UI component addition.

---
+++ src/app/render.js
@@ -0,0 +1,8 @@
+import addFavicon from "./components/favicon";
+import addLogo from "./components/logo";
+import "../main.scss";
+
+export default function render() {
+  addFavicon();
+  addLogo();
+}
