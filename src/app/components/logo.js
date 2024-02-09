e4bd6d7bb53d4eca43a3da9c235b59d318ad78ea
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2024-02-09 18:05:49 +0530

    Remove image tag and inject raw SVG into logo wrapper

--- src/app/components/logo.js
+++ src/app/components/logo.js
@@ -1,15 +1,11 @@
 import logo from "../../assets/icons/logo.svg";
 
-const img = document.createElement("img");
 const span = document.createElement("span");
 const logoWrapper = document.getElementById("logo-w");
 
 export default function addLogo() {
   span.id = "logo-text";
   span.textContent = "WeathIn";
-  img.alt = "weaIn";
-  img.src = logo;
-  img.id = "logo-fig";
-  logoWrapper.appendChild(img);
+  logoWrapper.innerHTML = logo;
   logoWrapper.appendChild(span);
 }
