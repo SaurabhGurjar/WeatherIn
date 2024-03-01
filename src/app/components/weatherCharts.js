f326b8015b33d2b50b418b7423e51a8aedf2dddf
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2024-03-01 22:06:27 +0530

    Change: constant name and use chart.canvas property instead of using canvas element in appendChild method.

--- src/app/components/weatherCharts.js
+++ src/app/components/weatherCharts.js
@@ -1,11 +1,11 @@
 import { Chart } from "chart.js/auto";
 
 export default function makeChart(temps, hours, chartLabel) {
-  const ctx = document.createElement("canvas");
+  const canvas = document.createElement("canvas");
   const chartWrapper = document.createElement("div");
   chartWrapper.id = "chart";
-  ctx.id = "hourly-weather-graph";
-  const chart = new Chart(ctx, {
+  canvas.id = "hourly-weather-graph";
+  const chart = new Chart(canvas, {
     type: "line",
     data: {
       labels: hours,
@@ -15,11 +15,11 @@ export default function makeChart(temps, hours, chartLabel) {
           data: temps,
           fill: false,
           borderColor: "rgb(75, 192, 192)",
-          tension: 0.4,
+          tension: 0.5,
         },
       ],
     },
   });
-  chartWrapper.appendChild(ctx);
+  chartWrapper.appendChild(chart.canvas);
   return chartWrapper;
 }
