3171f90337de92c107aae8cebf38d9772a9ace4c
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2024-03-01 18:29:23 +0530

    Add: Function which renders the setting page.

--- src/app/utils/renderPages.js
+++ src/app/utils/renderPages.js
@@ -1,7 +1,11 @@
 import homePage from "../views/home";
+import settingsPage from "../views/settings";
 import { $ } from "./DOMElements";
 
 export function renderHome() {
   $("main-c").innerHTML = homePage();
 }
 
+export function renderSettings() {
+  $("main-c").innerHTML = settingsPage();
+}
