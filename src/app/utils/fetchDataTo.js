05465536c5ee9f5b1fd6e8a26cb8d2a235550b9d
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2024-03-01 18:19:21 +0530

    Add: Icons to application.

--- src/app/utils/fetchDataTo.js
+++ src/app/utils/fetchDataTo.js
@@ -1,14 +1,34 @@
 import { $ } from "./DOMElements";
 import makeChart from "../components/weatherCharts";
+import Icons from "../components/weatherIcons";
+import renderSun from "./renderCanvas";
 
+export const icons = new Icons();
+
+// Update right section
 export async function rightSectionUI(response) {
   const wd = await response;
-  // Right side update
+  if (wd === null) {
+    return;
+  }
+
+  let astroIcon;
+  renderSun(wd);
+
+  if ((await wd.astroIcon()) === "sun") {
+    astroIcon = icons.sunIcon;
+  } else if ((await wd.astroIcon()) === "star") {
+    astroIcon = icons.starIcon;
+  } else {
+    astroIcon = icons.moonPhase[await wd.astroIcon()];
+  }
+  $("astro-icon").innerHTML = astroIcon;
   $("day-state").innerText = await wd.dayOrNight();
   $("region").innerText = `${await wd.cityName()}, ${await wd.stateName()}`;
-  $("temps").innerText = await wd.currentTempInCelsius();
+  $("temps").innerText = await wd.currentTemp();
 
-  const uv = await wd.todayUVData();
+  const uv = await wd.currentUVData();
+  $("ris-icon-wrapper").innerHTML = icons.UV[uv];
   $("ris-uv-data").innerText = `${uv}UV`;
   $("ris-ui-tag-text").innerText = await wd.uvLevel();
   $("ris-uv-text").innerText = `${await wd.uvLevel()} risk from UV rays`;
@@ -19,32 +39,45 @@ export async function rightSectionUI(response) {
   $("3-ris-weather-card-date").innerText = await wd.date(2);
 
   $("1-ris-weather-card-temps-text").innerText =
-    `${await wd.dayMaxTempInCelsius(0)}° / ${await wd.dayMinTempInCelsius(0)}°`;
+    `${await wd.dayMaxTemp(0)}° / ${await wd.dayMinTemp(0)}°`;
   $("2-ris-weather-card-temps-text").innerText =
-    `${await wd.dayMaxTempInCelsius(1)}° / ${await wd.dayMinTempInCelsius(1)}°`;
+    `${await wd.dayMaxTemp(1)}° / ${await wd.dayMinTemp(1)}°`;
   $("3-ris-weather-card-temps-text").innerText =
-    `${await wd.dayMaxTempInCelsius(2)}° / ${await wd.dayMinTempInCelsius(2)}°`;
+    `${await wd.dayMaxTemp(2)}° / ${await wd.dayMinTemp(2)}°`;
 
+  $("1-ris-weather-card-icon").innerHTML =
+    icons.conditions[await wd.conditionCode(0)];
+  $("2-ris-weather-card-icon").innerHTML =
+    icons.conditions[await wd.conditionCode(1)];
+  $("3-ris-weather-card-icon").innerHTML =
+    icons.conditions[await wd.conditionCode(2)];
   $("1-ris-weather-card-condition").innerText = await wd.condition(0);
   $("2-ris-weather-card-condition").innerText = await wd.condition(1);
   $("3-ris-weather-card-condition").innerText = await wd.condition(2);
 }
 
+// Update home page
 export async function homeUI(response) {
   const wd = await response;
-  const [hours, temps] = await wd.hourlyWeatherInCelsius();
+  if (wd === null) {
+    return;
+  }
+  const [hours, temps] = await wd.hourlyWeather();
+
+  $("wci").innerHTML = icons.conditions[await wd.currentConditionCode()];
   $("place-name").innerText = await wd.city();
   $("country").innerText = await wd.country();
-  $("card-temp").innerText = await wd.currentTempInCelsius();
-  $("max-temp").innerText = await wd.todayMaxTempInCelsius();
+  $("card-temp").innerText = await wd.currentTemp();
+  $("max-temp").innerText = await wd.todayMaxTemp();
   $("condition-text").innerText = await wd.currentCondition();
   $("data-pressure").innerText = await wd.currentPressureInmb();
-  $("data-visibilty").innerText = await wd.currentVisiblityInKm();
+  $("data-visibilty").innerText = await wd.currentVisiblity();
   $("data-humidity").innerText = await wd.currentHumidity();
   $("aqi").innerText = await wd.currentAirQuality();
   $("wind-direction").innerText = await wd.currentWindDirection();
   $("quality-bar-fill").style.width = await wd.airQualityIndicator();
-
+  $("tomorrow-weather-card-icon").innerHTML =
+    icons.conditions[await wd.conditionCode(1)];
   if ($("chart") !== null) {
     $("chart").remove();
     $("temp-chart-wrapper").appendChild(
@@ -55,6 +88,6 @@ export async function homeUI(response) {
       makeChart(temps, hours, "Temperature in °C"),
     );
   }
-  $("tomorrow-card-temp").innerText = await wd.tomorrowTempInCelsius();
+  $("tomorrow-card-temp").innerText = await wd.tomorrowTemp();
   $("tomorrow-weather").innerText = await wd.tomorrowCondition();
 }
