376121c1ed02909e1a6f34d802dc4970eae231e3
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2024-03-01 18:23:11 +0530

    Add: Error catching to the get DOM element functions.

--- src/app/utils/DOMElements.js
+++ src/app/utils/DOMElements.js
@@ -1,9 +1,17 @@
 export function $(id) {
-  return document.getElementById(id);
+  try {
+    return document.getElementById(id);
+  } catch (e) {
+    throw new Error(e);
+  }
 }
 
 export function $$(name, selector) {
+  try {
     if (selector === "class") return document.querySelectorAll(`.${name}`);
     if (selector === "id") return document.querySelector(`#${name}`);
     return document.querySelector(name);
+  } catch (e) {
+    throw new Error(e);
+  }
 }
