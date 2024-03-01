37e8a38070fc335c451ab80efc6fdbb79f16c610
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2024-03-01 20:28:25 +0530

    Remove: Unnecessary variable assignment and directly return promise.

--- src/app/utils/getWeather.js
+++ src/app/utils/getWeather.js
@@ -1,6 +1,5 @@
 import WeatherData from "./data";
 
 export default async function getWeatherData(search) {
-    const wdData = new WeatherData(4, search);
-    return wdData;
-  }
+  return new WeatherData(4, search);
+}
